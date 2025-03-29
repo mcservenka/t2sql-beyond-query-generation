aa830ad2f70d3958ec7536a376b2460eeb532415
mcservenka <87618313+mcservenka@users.noreply.github.com>
2025-03-29 11:57:08 +0100

    Add files via upload

--- generate.py
+++ generate.py
@@ -22,25 +22,3 @@ if __name__ == '__main__':
     pred.generate_results(overwrite=False)
 
     print("Results generated.")
-
-
-
-# ask llm
-#gpt4o_spider = OpenAIAPI(dataset="spider", model="gpt-4o-2024-08-06")
-#gpt35_spider = OpenAIAPI(dataset="spider", model="gpt-3.5-turbo-0125")
-#gpt4o_bird = OpenAIAPI(dataset="bird", model="gpt-4o-2024-08-06")
-#gpt4o_bird.generate_results(overwrite=False)
-#gpt4o_bird.prep_evaluation()
-
-
-#"meta-llama/Llama-3.3-70B-Instruct-Turbo"
-#"Qwen/Qwen2.5-Coder-32B-Instruct"
-#qwen_spider = OpenAIAPI(dataset="bird", provider="together", model="Qwen/Qwen2.5-Coder-32B-Instruct")
-#qwen_spider.generate_results(overwrite=False)
-#qwen_spider.prep_evaluation()
-
-# evaluate results
-
-#eval = Evaluator(dataset="bird", model="Qwen/Qwen2.5-Coder-32B-Instruct")
-#eval.evaluate_bird()
-
